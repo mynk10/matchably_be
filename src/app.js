868e5057ed8744f9c834f3646406549745b3b426
mynk10 <mynk1008@gmail.com>
2025-05-06 23:42:21 +0530

    testing get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@ const express = require("express");
 
 const app = express(); //making an instance of an express server
 
-//request handler 
-app.use("/hello", (req, res) => {
-  res.send("hello from the server");
+//request handler
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "mayank", lastname: "sharma" });
 });
 
-app.use("/test", (req, res) => {
-  res.send("hello this is test");
+app.post("/user", (req, res) => {
+  res.send("the details of user is saved");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("the details of user is deleted");
 });
 
 app.use("/", (req, res) => {
   res.send("hello this is dashboard");
 });
 
-//listening to port 
+//listening to port
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
 });

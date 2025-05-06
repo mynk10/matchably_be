64961da86c1ddc1b1d7c33be7a35506f4c417b8a
mynk10 <mynk1008@gmail.com>
2025-05-07 02:16:28 +0530

    using next()

--- src/app.js
+++ src/app.js
@@ -4,21 +4,18 @@ const app = express(); //making an instance of an express server
 
 //request handler
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "mayank", lastname: "sharma" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("the details of user is saved");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("the details of user is deleted");
-});
-
-app.use("/", (req, res) => {
-  res.send("hello this is dashboard");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route handler");
+    next();
+    // res.send("route handler 1");
+  },
+  (req, res) => {
+    console.log("handling the route handler 2");
+    res.send("route handler 2")
+  }
+);
 
 //listening to port
 app.listen(3000, () => {
